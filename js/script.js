040f1e72c59468247b494cf8ba2a51f1ba47d916
Landix <soporte.landix@gmail.com>
2025-08-07 13:00:38 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuOpenIcon = document.getElementById('menu-open');
+    const menuCloseIcon = document.getElementById('menu-close');
+
+    if (menuToggle && mobileMenu && menuOpenIcon && menuCloseIcon) {
+        menuToggle.addEventListener('click', function() {
+            mobileMenu.classList.toggle('hidden');
+            menuOpenIcon.classList.toggle('hidden');
+            menuCloseIcon.classList.toggle('hidden');
+        });
+    }
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+    const scrollThreshold = 50;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > scrollThreshold) {
+            navbar.classList.add('bg-black');
+        } else {
+            navbar.classList.remove('bg-black');
+        }
+    });
+
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentItem = 0;
+
+    function showNextItem() {
+        carouselItems[currentItem].classList.add('hidden');
+        currentItem = (currentItem + 1) % carouselItems.length;
+        carouselItems[currentItem].classList.remove('hidden');
+    }
+
+    setInterval(showNextItem, 5000);
+});
